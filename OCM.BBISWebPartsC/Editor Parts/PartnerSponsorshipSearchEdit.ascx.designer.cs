f7f9b93a4855228f39f96355b341cffcc54489bc
Cary Mayeda <cmayeda@ba.org>
2015-03-10 15:17:52 -0600

    Update from Merge

--- OCM.BBISWebPartsC/Editor Parts/PartnerSponsorshipSearchEdit.ascx.designer.cs
+++ OCM.BBISWebPartsC/Editor Parts/PartnerSponsorshipSearchEdit.ascx.designer.cs
@@ -10,7 +10,16 @@
 namespace OCM.BBISWebParts {
     
     
-    public partial class SponsorshipSearchEditOCMPartners {
+    public partial class PartnerSponsorshipSearchEdit {
+        
+        /// <summary>
+        /// txtPartnerLookupID control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPartnerLookupID;
         
         /// <summary>
         /// txtDocType control.
@@ -29,14 +38,5 @@ namespace OCM.BBISWebParts {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtResultsPerPage;
-        
-        /// <summary>
-        /// txtPartnerLookupId control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtPartnerLookupId;
     }
 }
